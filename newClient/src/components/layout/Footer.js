d7313ffbcd0d542baf75448e4644fcbe43c2eebe
Justin Liwag <justin@sleightdesigns.com>
2018-11-12 15:35:59 -0800

    add new client folder

---
+++ newClient/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <section className="foot">
+        <div className="container">
+          <p>HAND-BUILT IN PASADENA, CALIFORNIA © 2018</p>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Footer;
