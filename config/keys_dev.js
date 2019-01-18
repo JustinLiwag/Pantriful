a85201bf314813a38f135090708b19ef2b8dd376
Justin Liwag <justin@sleightdesigns.com>
2019-01-18 15:52:28 -0800

    Restructure file structure

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb://justin:justinpantriful@pantriful-app-dev-shard-00-00-uvvvv.mongodb.net:27017,pantriful-app-dev-shard-00-01-uvvvv.mongodb.net:27017,pantriful-app-dev-shard-00-02-uvvvv.mongodb.net:27017/test?ssl=true&replicaSet=pantriful-app-dev-shard-0&authSource=admin&",
+  secretOrKey: "1029fsiodfj0asd"
+};
