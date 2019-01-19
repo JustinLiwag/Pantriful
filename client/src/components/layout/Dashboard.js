4771b27143728c003724079d2359b1a181814c98
Justin Liwag <justin@sleightdesigns.com>
2019-01-18 16:20:28 -0800

    Fix dashboard message

--- client/src/components/layout/Dashboard.js
+++ client/src/components/layout/Dashboard.js
@@ -6,10 +6,9 @@ class Dashboard extends Component {
       <div className="container dashboard">
         <h1>Welcome to Pantriful</h1>
         <p>
-          Thank you for signing up! We will be launching our beta on soon and
-          begin opening up profiles to test. We can't wait to show you what we
-          have been working on. We are excited to show you all what we have been
-          working on <br />
+          Thank you for signing up! We will be launching our beta soon and begin
+          opening up profiles to test. We can't wait to show you what we have
+          been working on. <br />
           <br />
           -The Pantriful Team
         </p>
