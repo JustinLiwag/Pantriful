9a8480c2af948379983393046b0ecd7dfc34871b
Justin Liwag <justin@sleightdesigns.com>
2019-01-22 09:06:06 -0800

    Add create food profile route

--- client/src/App.js
+++ client/src/App.js
@@ -12,10 +12,11 @@ import PrivateRoute from "./components/common/PrivateRoute";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-import Dashboard from "./components/dashboard/Dashboard";
 import Footer from "./components/layout/Footer";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-food-profile/CreateProfile";
 
 import "./App.css";
 
@@ -53,6 +54,11 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
             <Footer />
