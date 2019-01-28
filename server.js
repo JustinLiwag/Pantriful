f6d9b417c99ad11863d54778a96c502216c095be
Justin Liwag <justin@sleightdesigns.com>
2019-01-28 10:40:35 -0800

    fix ssl redirect

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require("path");
-const sslRedirect = require(‘heroku-ssl-redirect’);
+const sslRedirect = require("heroku-ssl-redirect");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
