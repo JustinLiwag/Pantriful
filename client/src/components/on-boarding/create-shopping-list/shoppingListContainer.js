a11d3aad53004f275fcf8056a735fb102985bc6b
Justin Liwag <justin@sleightdesigns.com>
2019-03-11 20:25:39 -0700

    Fix navbar and styling for shoppingListContainer component

--- client/src/components/on-boarding/create-shopping-list/shoppingListContainer.js
+++ client/src/components/on-boarding/create-shopping-list/shoppingListContainer.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
-import Navbar from "../../layout/Navbar"
+import {Link} from 'react-router-dom'
 
 class ShoppingListContainer extends Component {
   render() {
     return (
-      <div className="container">
-        <Navbar />
-        <h1>Shopping List Container</h1>
+      <div>
+        <div className="on-boarding-nav">
+            <Link to="/"><button>Back</button></Link>
+            <Link to="/">
+              <img src="images/pantriful-logo-orange.png" alt="" />
+            </Link>
+        </div>
+        <div className="on-boarding-container container">
+          <h1>Shopping List Container</h1>
+        </div>
       </div>
     );
   }
