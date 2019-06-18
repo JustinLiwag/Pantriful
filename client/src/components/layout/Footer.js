a91d3fa2e00a20e105562c123c58d6637bf6926a
Justin Liwag <justin@pantriful.com>
2019-06-17 17:02:08 -0700

    Add new landing, login, and registration pages

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -4,30 +4,31 @@ import {Link} from "react-router-dom";
 class Footer extends Component {
   render() {
     return (
-      <div>
-        <section className="quote container">
-              <p>“You don’t have to cook fancy or complicated masterpieces - just good food from fresh ingredients.”<br /><br />-
-                  Julia Child</p>
-        </section>
-        <section className="footer ">
-              <div className="container">
-                  <Link className="footer-img" to="/"><img src="images/logo/pantriful-logo.png" alt=""/></Link>
-                  <p className="footer-note">Made with love in Pasadena, Ca</p>
-                  <img className="footer-logo" src="images//footer/footer-logo.png" alt=""/>
+      <footer className="bg-orange-base pt-12 pb-4 text-white">
+          <div className="container">
+            <div className="md:flex justify-between px-12 md:px-4 md:p-4">
+              <div className="mb-4 md:mb-0">
+                <h3 className="text-3xl font-extrabold italic text-left">Pantriful</h3>
+                <p>Your own smart personal grocery shopper</p>
               </div>
-              <div className="footer-menu">
-                  <h3>Menu</h3>
-                  <p><Link to="/">Home</Link></p>
-                  <p><Link to="/register">Sign Up</Link></p>
-                  <p><Link to="/login">Login</Link></p>
+              <div className="mb-4 md:mb-0">
+                <h4 className="mb-4 font-extrabold">Menu</h4>
+                <p><Link to="/">Home</Link></p>
+                <p><Link to="/register">Sign Up</Link></p>
+                <p><Link to="/login">Login</Link></p>
               </div>
-              <div className="footer-contact">
-                  <h3>Contact Us</h3>
-                  <p>hello@pantriful.com</p>
-                  <p>Pasadena, California</p>
+              <div>
+                <h4 className="mb-4 font-extrabold">Contact Us</h4>
+                <p>hello@pantriful.com</p>
+                <p>Pasadena, California</p>
               </div>
-        </section>
-      </div>
+            </div>
+            <div className="mt-12 p-4 md:pt-8 p-4 md:flex justify-between border-t border-white w-5/6 md:w-full mx-auto text-center">
+              <p>© 2019 Pantriful</p>
+              <p>Made in Pasadena, CA</p>
+            </div>
+          </div>
+        </footer>
     );
   }
 }
